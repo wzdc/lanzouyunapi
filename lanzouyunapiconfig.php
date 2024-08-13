a0fbbe520f93c96241f3c25af8a54dea4c7f1a5c
无知的错 <2067547250@qq.com>
2024-08-13 19:37:28 +0800

    Update lanzouyunapiconfig.php

--- lanzouyunapiconfig.php
+++ lanzouyunapiconfig.php
@@ -1,7 +1,7 @@
 <?php
 //缓存配置（需要安装apcu扩展）
 $cacheconfig=array(
-    "cache"        => 1,      // int    文件链接缓存开关（0关，1开）
+    "cache"        => 0,      // int    文件链接缓存开关（0关，1开）
     "cacheexpired" => 2000,   // int    文件链接缓存时间（秒，当无法获取到过期时间时使用）
     "foldercache"  => 0,      // int    缓存文件夹开关
 );
